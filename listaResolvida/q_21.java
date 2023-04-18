e28952f679f2f1d4a2c773f5d266194550da8401
Thalita Rodrigues <112733274+thalitaasuzy@users.noreply.github.com>
2023-04-18 18:18:36 -0300

    Update q_21.java

--- listaResolvida/q_21.java
+++ listaResolvida/q_21.java
@@ -7,13 +7,18 @@ public class Principal {
 	public static void main(String[] args) {
 		/* (21) faça um programa para calcular e mostrar a que distância deve estar uma escada da parede. 
 		O usuário deve fornecer o tamanho da escada e a altura em que deseja pregar o quadro. */
-				Scanner sc = new Scanner (System.in) ;
-				System.out.println("Digite o tamanho da sua escada: ");
-			    double tamEscada = sc.nextDouble() ;
-			    System.out.println("Digite a altura que deseja pregar o quadro: ");
-			    double altura = sc.nextDouble() ;
-			    double distancia = Math.sqrt ((tamEscada * tamEscada) + (altura * altura)) ;  
-			    System.out.println("A distancia que a escada deve está é de: " + distancia);
+		
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Digite, em centímetros, o tamanho da escada: ");
+		double tamEscada = sc.nextDouble();
+		
+		System.out.println("Digite, em centímetros, a altura que se deseja pendurar o quadro: ");
+		double altura = sc.nextDouble();
+		
+		double distancia = Math.sqrt ((tamEscada * tamEscada) + (altura * altura)); 
+		
+		System.out.println("A distância entre a escada e a parede deve ser de " + distancia + "cm.");
 			    
 	}
 
