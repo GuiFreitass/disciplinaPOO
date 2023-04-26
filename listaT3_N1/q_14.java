55b7607c1faf9bed06e7140e13e5ac37c3bc442f
Thalita Rodrigues <112733274+thalitaasuzy@users.noreply.github.com>
2023-04-25 22:30:03 -0300

    Create q_14.java

---
+++ listaT3_N1/q_14.java
@@ -0,0 +1,25 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {		// Faça um progama que calcule a soma dos 50 primeiros números pares. Esse progama não recebe valor do teclado. Os primeiros numeros pares são: 2, 4, 6...
+
+				int soma = 0;
+
+			    for (int i = 2; i <= 100; i += 2) {
+
+			        soma += i;
+
+			    }
+
+			    System.out.println("A soma dos 50 primeiros números pares é: " + soma);
+
+				
+
+			}
+
+		
+
+	
+
+}
